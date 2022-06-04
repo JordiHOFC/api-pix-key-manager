8957822a3c627f63278d83c59ade49d961c3ee2e
jordihofc <jordyhenrique7@gmail.com>
2022-06-04 16:03:22 -0300

    ambiente pronto para iniciar projeto

---
+++ src/test/java/github/com/br/jordihofc/pix/PixApplicationTests.java
@@ -0,0 +1,13 @@
+package github.com.br.jordihofc.pix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
